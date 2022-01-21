afd4e74aab2fe5cf7862b932437272fc0b83a79a
canewsin <canews.in@gmail.com>
2022-01-21 21:43:54 +0530

    Added GitHub Action Support
    - rustfmt
    - cargo clippy fixes
    - added security audit

--- src/error.rs
+++ src/error.rs
@@ -4,36 +4,36 @@ use thiserror::Error;
 
 #[derive(Error, Debug, Eq, PartialEq)]
 pub enum CryptError {
-  #[error("Failed to decode signature")]
-  DecodeSignatureFailure,
-  #[error("Failed to recover ID")]
-  RecoveryIdFailure,
-  #[error("Failed to recover signature")]
-  RecoverableSignatureFailure,
-  #[error("Something wrong with the message")]
-  MessageFailure,
-  #[error("Public key somethng")]
-  PublicKeyFailure,
-  #[error("Address mismatch ({0})")]
-  AddressMismatch(String),
-  #[error("Private key failure")]
-  PrivateKeyFailure,
+    #[error("Failed to decode signature")]
+    DecodeSignatureFailure,
+    #[error("Failed to recover ID")]
+    RecoveryIdFailure,
+    #[error("Failed to recover signature")]
+    RecoverableSignatureFailure,
+    #[error("Something wrong with the message")]
+    MessageFailure,
+    #[error("Public key somethng")]
+    PublicKeyFailure,
+    #[error("Address mismatch ({0})")]
+    AddressMismatch(String),
+    #[error("Private key failure")]
+    PrivateKeyFailure,
 }
 
 impl From<DecodeError> for CryptError {
-  fn from(_: DecodeError) -> CryptError {
-    CryptError::DecodeSignatureFailure
-  }
+    fn from(_: DecodeError) -> CryptError {
+        CryptError::DecodeSignatureFailure
+    }
 }
 
 impl From<SecpError> for CryptError {
-  fn from(error: SecpError) -> CryptError {
-    match error {
-      SecpError::InvalidRecoveryId => CryptError::RecoveryIdFailure,
-      SecpError::InvalidSignature => CryptError::RecoverableSignatureFailure,
-      SecpError::InvalidMessage => CryptError::MessageFailure,
-      SecpError::InvalidPublicKey => CryptError::PublicKeyFailure,
-      _ => CryptError::RecoverableSignatureFailure,
+    fn from(error: SecpError) -> CryptError {
+        match error {
+            SecpError::InvalidRecoveryId => CryptError::RecoveryIdFailure,
+            SecpError::InvalidSignature => CryptError::RecoverableSignatureFailure,
+            SecpError::InvalidMessage => CryptError::MessageFailure,
+            SecpError::InvalidPublicKey => CryptError::PublicKeyFailure,
+            _ => CryptError::RecoverableSignatureFailure,
+        }
     }
-  }
 }
